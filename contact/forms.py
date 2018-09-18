9a142f0bcca0713144f87c9b292fe2c7969d0e2b
Junaid388 <junaidkhan.5a4@gmail.com>
2018-09-18 17:38:11 +0530

    Added views to create/edit blog post inside the site.

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,7 @@
-class MyModelForm(ModelForm):
+from django import forms
+from .models import Post
+
+class MyModelForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['Category']
+        fields = ['First_Name','Last_Name','Email_Address','Phone_Number','Category']
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import MyModelForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -12,4 +13,28 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'contact/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = MyModelForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = MyModelForm()
+    return render(request, 'contact/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = MyModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = MyModelForm(instance=post)
+    return render(request, 'contact/post_edit.html', {'form': form})
 
