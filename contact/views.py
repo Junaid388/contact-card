5f9b052065d3a000d21537e32ff8d0b1853e5aba
Junaid388 <junaidkhan.5a4@gmail.com>
2018-09-18 10:22:02 +0530

    Modified templates to display posts from database.

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'contact/post_list.html', {})
+    posts = Post.objects.order_by('First_Name')
+    return render(request, 'contact/post_list.html',{'posts': posts})
+
